bdde5810abbb70f1ea8f514f3d711e9b946a8ad0
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-01-20 17:50:41 +0600

    chore: read ALLOWED_HOSTS from env

--- webflix/settings.py
+++ webflix/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
