540b2087c9fcfa80b6127382aeebc097dab876ce
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-02-23 21:40:10 +0600

    chore: added STATIC_ROOT

--- webflix/settings.py
+++ webflix/settings.py
@@ -96,6 +96,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
